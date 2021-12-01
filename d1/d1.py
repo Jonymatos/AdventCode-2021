a60b5536cfcbf00be0dd406544f1e06e9a682022
João Rui Matos <joaoruimatos@gmail.com>
2021-12-01 23:22:08 +0000

    first commit

---
+++ d1/d1.py
@@ -0,0 +1,13 @@
+def count(lst, wd):
+    count=0
+    for i in range(wd, len(lst)):
+        if sum(lst[i-wd+1:i+1]) > sum(lst[i-wd:i]):
+            count+=1
+    return count
+
+
+file = open("example.txt", "r")
+list = [eval(num) for num in file.readlines()]
+file.close()
+print(count(list,1))
+print(count(list,3))
