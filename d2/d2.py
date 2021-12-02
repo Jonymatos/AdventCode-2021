16aade5f2b2a7084e0aa4974ed23b571fe512185
João Rui Matos <joaoruimatos@gmail.com>
2021-12-02 17:37:57 +0000

    d2.py Improved

--- d2/d2.py
+++ d2/d2.py
@@ -1,25 +1,24 @@
-def count(WithAim):
-    horizontal=0
-    depth=0
-    aim=0
-    file = open("input2.txt", "r")
-    while True:
-        line = file.readline()
-        step = line.split()
-        if len(line) == 0:
-            break
+def count(inp, WithAim):
+    horizontal, depth, aim = 0, 0, 0
+    for step in inp:
         units = eval(step[1])
+
         if step[0] == 'forward':
             horizontal += units
             depth += units * aim
+
         elif step[0] == 'up':
             depth -= units * int(not WithAim)
             aim -= units * int(WithAim)
+
         elif step[0] == 'down':
             depth += units * int(not WithAim)
             aim += units * int(WithAim)
-    file.close()
+            
     return horizontal*depth
 
-print(count(False)) #Part1
-print(count(True)) #Part2
+file = open("input2.txt", "r")
+inp = [num.split() for num in file.readlines()]
+file.close()
+print(count(inp, False)) #Part1
+print(count(inp, True)) #Part2
